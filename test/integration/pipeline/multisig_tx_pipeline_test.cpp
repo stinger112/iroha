ecc5c88adea3ef024e560a3b2a761a4c26d958a6
Kitsu <mail@kitsu.me>
2017-12-22 03:47:21 +0300

    Properly init irohad in mst integrations
    Signed-off-by: Kitsu <mail@kitsu.me>

--- test/integration/pipeline/multisig_tx_pipeline_test.cpp
+++ test/integration/pipeline/multisig_tx_pipeline_test.cpp
@@ -19,24 +19,38 @@
 #include <gtest/gtest.h>
 #include "crypto/keys_manager_impl.hpp"
 #include "framework/integration_framework/integration_test_framework.hpp"
+#include "model/generators/command_generator.hpp"
+#include "model/model_crypto_provider_impl.hpp"
+
+iroha::model::Transaction addSignature(const iroha::model::Transaction &t,
+                                       iroha::keypair_t keypair) {
+  iroha::model::Transaction tx = t;
+  iroha::model::ModelCryptoProviderImpl(keypair).sign(tx);
+  return tx;
+}
 
 /**
  * @given create peer with default genesis block
  * @when sent valid transaction with quorum 2 and one signature
- * AND sent same transaction with another signature
+ *   AND sent same transaction with another signature
  * @then check that block contains transaction
  */
 TEST(MST, OnePeerSendsTest) {
   // TODO 21/12/2017 muratovv rework with auto erased crypto manager
-  auto seed = iroha::create_seed("foo");
-  auto key_pair = iroha::create_keypair(seed);
+  auto user1 = "user1", user2 = "user2";
+  auto key_user1 = iroha::create_keypair(), key_user2 = iroha::create_keypair();
+  iroha::model::generators::CommandGenerator gen;
+
+  iroha::model::Transaction tx;
+  tx.commands.push_back(gen.generateAddAssetQuantity(
+      user1, "test", iroha::Amount().createFromString("0").value()));
 
   integration_framework::IntegrationTestFramework()
-      .setInitialState(
-          key_pair,
-          iroha::model::Block{})  // todo replace with valid genesis block
-      .sendTx(iroha::model::Transaction{})  // todo replace with tx with sign 1
-      .sendTx(iroha::model::Transaction{})  // todo replace with tx with sign 2
+      .setInitialState()  // todo replace with valid genesis block
+      .addUser(user1, key_user1)
+      .addUser(user2, key_user2)
+      .sendTx(addSignature(tx, key_user1))  // todo replace with tx with sign 1
+      .sendTx(addSignature(tx, key_user2))  // todo replace with tx with sign 2
       .skipProposal()
       .checkBlock([](const auto &block) {
       })  // todo check that transactions with both signatures appears
