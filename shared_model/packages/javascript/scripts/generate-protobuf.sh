9ea7860b641fc7b904bd34bf81f607b9693977c6
Arseniy Fokin <stinger112@gmail.com>
2018-03-20 14:24:38 +0700

    Generate protobufs in single line
    Signed-off-by: Arseniy Fokin <stinger112@gmail.com>

--- shared_model/packages/javascript/scripts/generate-protobuf.sh
+++ shared_model/packages/javascript/scripts/generate-protobuf.sh
@@ -5,9 +5,12 @@ IROHA_HOME="$(dirname $(dirname $(dirname $(dirname "${CURDIR}"))))"
 
 echo -------------------------
 echo "IROHA_HOME: $IROHA_HOME"
-echo "Generating JS protobuff files..."
+echo "Generating Protobuf JS files..."
 
-# Generating protobuff files
+# Generating protobuf files
 mkdir -p pb/grpc
-./node_modules/.bin/grpc_tools_node_protoc --proto_path=$IROHA_HOME/schema --js_out=import_style=commonjs,binary:./pb block.proto primitive.proto commands.proto queries.proto responses.proto endpoint.proto
-./node_modules/.bin/grpc_tools_node_protoc --proto_path=$IROHA_HOME/schema --js_out=import_style=commonjs,binary:./pb/grpc --plugin=protoc-gen-grpc=./node_modules/grpc-tools/bin/grpc_node_plugin --grpc_out=./pb endpoint.proto yac.proto ordering.proto loader.proto
+./node_modules/.bin/grpc_tools_node_protoc --proto_path=$IROHA_HOME/schema \
+--plugin=protoc-gen-grpc=./node_modules/grpc-tools/bin/grpc_node_plugin \
+--js_out=import_style=commonjs,binary:./pb \
+--grpc_out=./pb \
+endpoint.proto yac.proto ordering.proto loader.proto block.proto primitive.proto commands.proto queries.proto responses.proto
