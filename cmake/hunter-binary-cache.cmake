eb36b853b0234aac287756a30c26c2dca1c5abaf
Arseniy Fokin <stinger112@gmail.com>
2018-03-26 16:45:00 +0700

    Remove Hunter Binary Cache support
    Signed-off-by: Arseniy Fokin <stinger112@gmail.com>

--- cmake/hunter-binary-cache.cmake
+++
@@ -1,8 +0,0 @@
-hunter_upload_password(
-    REPO_OWNER "soramitsu"
-    REPO       "hunter-binary-cache"
-    # USER = <your user>
-    USERNAME   "$ENV{GITHUB_USER}"
-    # PASSWORD = GitHub token saved as a secure environment variable
-    PASSWORD   "$ENV{GITHUB_USER_PASSWORD}" # it is actually token
-)
