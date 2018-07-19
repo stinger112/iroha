35c6bbaf4a2d76fb25e06a006b07087e0c04001d
Arseniy Fokin <stinger112@gmail.com>
2018-07-20 00:33:15 +0700

    Fix protobuf generating
    Signed-off-by: Arseniy Fokin <stinger112@gmail.com>

--- shared_model/packages/javascript/scripts/generate-protobuf.sh
+++ shared_model/packages/javascript/scripts/generate-protobuf.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 CURDIR="$(cd "$(dirname "$0")"; pwd)"
-IROHA_HOME="$(dirname $(dirname $(dirname $(dirname "${CURDIR}"))))"
+SHARED_MODEL_HOME="$(dirname $(dirname $(dirname "${CURDIR}")))"
 
 # Check if we inside Iroha repository
-if [ -d "$IROHA_HOME/schema" ]; then
+if [ -d "${SHARED_MODEL_HOME}/schema" ]; then
 	echo "Generating Protobuf JS files..."
 	
-	./node_modules/.bin/grpc_tools_node_protoc --proto_path=$IROHA_HOME/schema \
+	./node_modules/.bin/grpc_tools_node_protoc --proto_path=${SHARED_MODEL_HOME}/schema \
 	--plugin=protoc-gen-grpc=./node_modules/grpc-tools/bin/grpc_node_plugin \
-	--js_out=import_style=commonjs,binary:./pb \
-	--grpc_out=./pb \
-	endpoint.proto yac.proto ordering.proto loader.proto block.proto primitive.proto commands.proto queries.proto responses.proto
+	--js_out=import_style=commonjs,binary:${CURDIR}/../pb \
+	--grpc_out=${CURDIR}/../pb \
+	block.proto commands.proto endpoint.proto primitive.proto proposal.proto qry_responses.proto queries.proto transaction.proto
+else
+  echo "This is standalone installation. No protobufs generated."
 fi
